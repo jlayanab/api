cf14b2bc451f4fc8cde012408ec1867259d2437b
Jorge E. Layana <jlayanab@icloud.com>
2020-08-12 11:58:03 -0500

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
